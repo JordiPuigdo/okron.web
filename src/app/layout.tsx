8b7937eed2b875fabf0c1e83d1e0ff14cb068367
JordiPuigdo <jordi.puigdollers@holaglow.com>
2025-07-09 11:53:10 +0200

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -6,7 +6,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="max-h-screen h-full bg-white text-hg-black">
+    <html
+      lang="en"
+      translate="no"
+      className="max-h-screen h-full bg-white text-hg-black"
+    >
       <body>{children}</body>
     </html>
   );
