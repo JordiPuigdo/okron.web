61d835633e796a933a80b212f8d38c0b507fa947
JordiPuigdo <jordi.puigdollers@holaglow.com>
2025-09-25 15:07:10 +0200

    add

--- src/app/(pages)/print/deliveryNote/components/DeliveryNoteHeader.tsx
+++ src/app/(pages)/print/deliveryNote/components/DeliveryNoteHeader.tsx
@@ -1,7 +1,6 @@
 import { SystemConfiguration } from 'app/interfaces/Config';
 import { DeliveryNote } from 'app/interfaces/DeliveryNote';
 import dayjs from 'dayjs';
-import Image from 'next/image';
 
 export const DeliveryNoteHeader = ({
   deliveryNote,
@@ -13,9 +12,9 @@ export const DeliveryNoteHeader = ({
   const company = config.company;
 
   return (
-    <div className="space-y-2 rounded-lg border border-gray-200 print:border-0 print:p-2">
+    <div className="rounded-lg border border-gray-200 print:border-0 print:p-2 ">
       {/* Logo and Document Title */}
-      <div className="flex justify-between items-end">
+      <div className="flex justify-between items-end pb-4">
         <div className="w-48 h-16 relative">
           <img
             src={company.urlLogo}
@@ -38,94 +37,67 @@ export const DeliveryNoteHeader = ({
           </div>
         </div>
       </div>
-
       {/* Company and Client Information */}
-      <div className="grid grid-cols-2 gap-6">
+      <div className="flex pb-4">
         {/* Company Information */}
-        <div className="p-4 bg-gray-50 rounded-lg flex flex-col h-60">
-          {/* Altura fija */}
-          <h2 className="text-sm font-semibold text-gray-800 mb-2 border-b bg-gray-50 border-gray-200">
-            Informació de l'Empresa
-          </h2>
-          <div className="flex flex-col flex-grow justify-between bg-gray-50">
-            {/* Primera sección: Nombre de la empresa */}
-            <div>
-              <p className="font-medium text-gray-900">{company.fiscalName}</p>
-            </div>
 
-            {/* Segunda sección: Dirección completa */}
-            <div className="text-sm text-gray-700 space-y-1">
-              <p className="line-clamp-2">{company.address.address}</p>
-              <p>
-                {company.address.postalCode}, {company.address.city}
-              </p>
-              <p>{company.address.province}</p>
-            </div>
+        <div className="flex flex-col flex-grow justify-between ">
+          {/* Primera sección: Nombre de la empresa */}
+          <div>
+            <p className="font-medium text-gray-900">{company.fiscalName}</p>
+          </div>
 
-            {/* Tercera sección: NIF y contacto (alineado al bottom) */}
-            <div className="pt-2 border-t border-gray-100">
-              <p className="text-sm text-gray-600">
-                <span className="font-medium">NIF:</span> {company.nif}
-              </p>
-              <p className="text-sm text-gray-600">
-                <span className="font-medium">Tel:</span> {company.phone}
-              </p>
-              <p className="text-sm text-gray-600 break-all">
-                <span className="font-medium">Email:</span> {company.email}
-              </p>
-            </div>
+          {/* Segunda sección: Dirección completa */}
+          <div className="text-sm text-gray-700 space-y-1">
+            <p className="line-clamp-2">{company.address.address}</p>
+            <p>
+              {company.address.postalCode}, {company.address.city}
+            </p>
+            <p>{company.address.province}</p>
+          </div>
+
+          {/* Tercera sección: NIF y contacto (alineado al bottom) */}
+          <div className="">
+            <p className="text-sm text-gray-600">{company.nif}</p>
+            <p className="text-sm text-gray-600">{company.phone}</p>
+            <p className="text-sm text-gray-600 break-all">{company.email}</p>
           </div>
         </div>
+      </div>
 
-        {/* Client Information */}
-        <div className="p-4 bg-blue-50 rounded-lg flex flex-col h-60">
-          {' '}
-          {/* Ajusta la altura según necesites */}
-          <h2 className="text-sm font-semibold text-gray-800 mb-2 border-b border-gray-200">
-            Informació del Client
-          </h2>
-          <div className="flex flex-col flex-grow justify-between">
-            {/* Primera sección: Información básica */}
-            <div className="space-y-2">
-              <p className="font-medium text-gray-900">
-                {deliveryNote.companyName}
-              </p>
-            </div>
+      {/* Client Information */}
+      <div className="flex flex-col border-t-2 border-black border-b-2 ">
+        <div className="flex flex-col flex-grow justify-between">
+          {/* Primera sección: Información básica */}
+          <div className="space-y-2">
+            <p className="font-medium text-gray-900">
+              {deliveryNote.companyName}
+            </p>
+          </div>
 
-            {/* Segunda sección: Dirección hasta provincia */}
-            <div className="text-sm text-gray-700 space-y-1">
-              <p className="flex">
-                <span className="font-medium shrink-0 mr-1">Adreça:</span>
-                <span className="line-clamp-2">
-                  {deliveryNote.customerAddress.address}
-                </span>
-              </p>
-              <p>
-                <span className="font-medium">CP:</span>{' '}
-                {deliveryNote.customerAddress.postalCode},{' '}
-                {deliveryNote.customerAddress.city}
-              </p>
-              <p>
-                <span className="font-medium">Província:</span>{' '}
-                {deliveryNote.customerAddress.province}
-              </p>
-            </div>
+          {/* Segunda sección: Dirección hasta provincia */}
+          <div className="text-sm text-gray-700 space-y-1">
+            <p className="flex">
+              <span className="line-clamp-2">
+                {deliveryNote.customerAddress.address}
+              </span>
+            </p>
+            <p>
+              {deliveryNote.customerAddress.postalCode},{' '}
+              {deliveryNote.customerAddress.city}
+            </p>
+            <p>{deliveryNote.customerAddress.province}</p>
+          </div>
 
-            {/* Tercera sección: NIF y contacto (alineado al bottom) */}
-            <div className="pt-2 border-t border-gray-100">
-              <p className="text-sm text-gray-600">
-                <span className="font-medium">NIF:</span>{' '}
-                {deliveryNote.customerNif}
-              </p>
-              <p className="text-sm text-gray-600">
-                <span className="font-medium">Tel:</span>{' '}
-                {deliveryNote.customerPhone}
-              </p>
-              <p className="text-sm text-gray-600 break-all">
-                <span className="font-medium">Email:</span>{' '}
-                {deliveryNote.customerEmail}
-              </p>
-            </div>
+          {/* Tercera sección: NIF y contacto (alineado al bottom) */}
+          <div className="">
+            <p className="text-sm text-gray-600">{deliveryNote.customerNif}</p>
+            <p className="text-sm text-gray-600">
+              {deliveryNote.customerPhone}
+            </p>
+            <p className="text-sm text-gray-600 break-all">
+              {deliveryNote.customerEmail}
+            </p>
           </div>
         </div>
       </div>
