f813c6d404c94500877fda2e779f851cdc67851c
Jordi <jordipuigdollers1992@gmail.com>
2026-03-14 12:34:42 +0100

    add

--- src/app/interfaces/CreditNote.ts
+++ src/app/interfaces/CreditNote.ts
@@ -58,7 +58,7 @@ export interface CreditNoteCreationRequest {
 export interface CreditNoteUpdateRequest {
   id: string;
   reason?: string;
-  customerId?: string;
+  customerId?: string | null;
   items?: CreditNoteItemRequest[];
 }
 
