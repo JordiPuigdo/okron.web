3e3ece31b4289c3fae84f33586323e72b4ac2ccb
Jordi <jordipuigdollers1992@gmail.com>
2026-04-14 09:06:09 +0200

    add

--- src/app/(pages)/assemblyBudgets/[id]/components/ArticlePreviewModal.tsx
+++ src/app/(pages)/assemblyBudgets/[id]/components/ArticlePreviewModal.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { ArticleType, ArticleWithFullTree } from 'app/interfaces/Article';
 import { useTranslations } from 'app/hooks/useTranslations';
-import { formatCurrencyServerSider } from 'app/utils/utils';
+import { ArticleType, ArticleWithFullTree } from 'app/interfaces/Article';
 import { ArticleService } from 'app/services/articleService';
+import { formatCurrencyServerSider } from 'app/utils/utils';
 import { Package, Wrench, X } from 'lucide-react';
 
 interface ArticlePreviewModalProps {
